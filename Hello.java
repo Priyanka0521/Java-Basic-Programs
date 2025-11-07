37493c87c2b07eff62d32857b1d352b320d26576
Priyanka0521 <dubbakapriyanka0521@gmail.com>
2025-11-07 11:13:53 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+class Hello
+{
+public static void main(String args[])
+{
+    int num1=0x7E;
+    byte by=127;
+    short sh=558;
+    long l=5658l;
+    float f=5.6f;
+    double d=5.8;
+    char c='k';
+    boolean b=true;
+    System.out.println(num1);
+    
+    }
+}
