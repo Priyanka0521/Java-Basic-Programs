2b41584c3570cb039e046e4c8dba0609cba44271
Priyanka0521 <dubbakapriyanka0521@gmail.com>
2025-11-07 11:14:17 +0530

    Add files via upload

---
+++ IfExample.java
@@ -0,0 +1,14 @@
+public class IfExample{
+    public static void main(String[] args){
+        int x=9;
+        int y=5;// Two integers
+        
+        if(x>y) //Conditional statements if if and else
+        {
+            System.out.println(x);
+            System.out.println("Hello");
+        }
+        else
+        System.out.println(y);
+    }
+}
