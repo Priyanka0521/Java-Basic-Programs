ce4aad67af4d3dc1755ab422c51ab081f7c92f52
Priyanka0521 <dubbakapriyanka0521@gmail.com>
2025-11-07 11:15:41 +0530

    Add files via upload

---
+++ LogicalOperator.java
@@ -0,0 +1,13 @@
+public class LogicalOperator{
+    public static void main(String[] args){
+        int x=9;
+        int y=5;
+        int a=5;
+        int b=9;
+        //boolean result = x>y && a<b;// both are true its true
+        //boolean result = x<y || a<b;// only one true its true
+        boolean result = a<b;// it false it will give you true (opposite symbols  )
+        System.out.println(!result);
+
+    }
+}
