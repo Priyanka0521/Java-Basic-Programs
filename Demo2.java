c1f09609960132e74b56bfeaeb9a37044a6e8771
Priyanka0521 <dubbakapriyanka0521@gmail.com>
2025-11-07 11:12:52 +0530

    Add files via upload

---
+++ Demo2.java
@@ -0,0 +1,8 @@
+public class Demo2{
+    public static void main(String[] args){
+        Double x=6.8;// Relational operators <,>,==,!=(not equal to),<=.>=
+        Double y=9.8;
+        boolean result = x<=y;
+        System.out.println(result);   
+         }
+}
